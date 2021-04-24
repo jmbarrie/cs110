92226a7296d4139c16990a49dc1b2be13e3c063b
Elise Lin <elin019@ucr.edu>
2021-04-24 14:42:17 -0700

    Functionality, done. Need to fix CSS

--- lab3/main.js
+++ lab3/main.js
@@ -14,6 +14,19 @@ Interesting JSON values:
 
 */
 
+function processDate(pre_date) {
+    // 2021-04-24T20:59:23+00:00
+    let post_date = pre_date.split('T');
+
+    return post_date[0];
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function create_elements(json) {
     /*
     Uses the JSON data from API to dynamically create tweet elements. 
@@ -23,39 +36,40 @@ function create_elements(json) {
     */
     let body = document.getElementsByClassName('flex-child content-center')[0];
 
-    for (let i = 0; i < json.length; i++) {
-        let tweetDiv = document.createElement('div');
-        let profilePic = document.createElement('img');
-        let userName = document.createElement('p');
-        let displayName = document.createElement('p');
-        let tweetText = document.createElement('p');
     
-        tweetDiv.className = 'tweet';
-        tweetDiv.id = json[i]._id;
+    let tweetDiv = document.createElement('div');
+    let profilePic = document.createElement('img');
+    let userName = document.createElement('p');
+    let displayName = document.createElement('p');
+    let tweetText = document.createElement('p');
+
+    tweetDiv.className = 'tweet';
+    tweetDiv.id = json._id;
+
+    profilePic.src = json.profile_pic;
+    profilePic.className = 'pfp_post';
+
+    tweetText.innerHTML = json.text;
+
+    userName.className = 'name';
+    userName.innerHTML = json.name;
+
+    displayName.className = 'grey';
     
-        profilePic.src = json[i].profile_pic;
-        profilePic.className = 'pfp_post';
-    
-        tweetText.innerHTML = json[i].text;
-    
-        userName.className = 'name';
-        userName.innerHTML = json[i].name;
-    
-        displayName.className = 'grey';
-        displayName.innerHTML = '@' + json[i].screen_name;
-    
-        // TODO: Figure out what is going on with userName and displayName in html
-        // There might be some playing we have to do with 'gray' and 'name' classes
-        tweetText.appendChild(userName);
-        tweetText.appendChild(displayName);
-        tweetDiv.appendChild(profilePic);
-        tweetDiv.appendChild(tweetText);
-        body.appendChild(tweetDiv);
-    }
+    displayName.innerHTML = '@' + json.screen_name + ' ' + processDate(json.creation_date);
+
+    // TODO: Figure out what is going on with userName and displayName in html
+    // There might be some playing we have to do with 'gray' and 'name' classes
+    tweetText.appendChild(userName);
+    tweetText.appendChild(displayName);
+    tweetDiv.appendChild(profilePic);
+    tweetDiv.appendChild(tweetText);
+    body.appendChild(tweetDiv);
 }
 
 function get_data(url) {
-    fetch(url)
+    if (!(document.getElementById("pause_button").checked)) {
+        fetch(url)
         .then(res => res.json())
         .then(data => {
             for (let i = 0; i < data["statuses"].length; i++) {
@@ -67,7 +81,7 @@ function get_data(url) {
                 // this presents issues if multiple mentions, hashtags, urls
                 if (!(data["statuses"][i]["_id"] in our_data)) {
                     our_data.push({
-                        "_id": data["statuses"][i]["_id"],
+                        "_id": data["statuses"][i]["_id"],  
                         "text": data["statuses"][i]["text"],
                         "name": data["statuses"][i]["user"]["name"],
                         "screen_name": data["statuses"][i]["user"]["screen_name"],
@@ -75,18 +89,26 @@ function get_data(url) {
                         "hashtags": data["statuses"][i]["entities"]["hashtags"],
                         "mentions": data["statuses"][i]["entities"]["user_mentions"],
                         "urls": data["statuses"][i]["entities"]["urls"],
-                        "creation_date": data["statuses"][i]["created_at"]
+                        "creation_date": data["statuses"][i]["created_at"],
+                        "tweeted": false
                     })
                 }
             }
             our_data.sort((a, b) => (a.creation_date > b.creation_date) ? 1 : -1);
-            create_elements(our_data);
+            for (let i = 0; i < our_data.length; ++i) {
+                if (!(our_data[i].tweeted)) {
+                    create_elements(our_data[i]);
+                    our_data[i].tweeted = true;
+                }
+            }
+            console.log(id_list);
             console.log(our_data);
             console.log(data);
         })
         .catch(err => {
             console.log(err)
         });
+    }
 }
 
 const api_url = "http://twitterfeedserverrails-env.eba-xmqy8ybh.us-east-1.elasticbeanstalk.com/feed/random?q=weather"
@@ -94,4 +116,21 @@ var our_data = []
 get_data(api_url);
 
 // Format is scope(window).setInterval(function, time (in ms), arguments)
-// window.setInterval(get_data, 10000, api_url);
+window.setInterval(get_data, 5000, api_url);
+
+
+// Search Bar Implementation
+let searchString = "" // here we use a global variable
+
+const handleSearch = event => {
+    searchString = event.target.value.trim().toLowerCase();
+    // 1. match all tweets containing search values 'live' OR after user hits enter
+    removeAllChildNodes(document.getElementsByClassName("flex-child content-center")[0]);
+    
+    for (let i = 0; i < our_data.length; ++i) {
+        if (our_data[i].text.toLowerCase().includes(searchString)) {
+            create_elements(our_data[i]);
+        }
+    }
+}
+document.getElementById("searchBar").addEventListener("input", handleSearch)

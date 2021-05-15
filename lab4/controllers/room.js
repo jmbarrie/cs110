4db5900dd1f0f13b116d2fb2a21dde5400b840bb
Juan Barrientos <jbarr078@ucr.edu>
2021-05-15 14:52:38 -0700

    Fixed issue with pulling/validating data

--- lab4/controllers/room.js
+++ lab4/controllers/room.js
@@ -5,37 +5,15 @@ const Chatroom = require('../models/Chatrooms');
 
 // Example for handle a get request at '/:roomName' endpoint.
 function getRoom(request, response){
-    // TODO: Validate for created rooms
-    // TODO: Room has already been created
-
-    // Room has not been created yet
     let roomName = request.params.roomName;
-    // Chatroom.exists({chatroom_name: roomName}, function (err, doc) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log('Result: ', doc);
-    //         boolCheck = doc;
-    //     }
-    // })
-    Chatroom.exists({chatroom_name: roomName}, function (err, doc) {
+    Chatroom.find({chatroom_name: roomName}, function (err, docs) {
         if (err) {
             console.log(err);
         } else {
-            // TODO: Figure out how to access data from within a mongoose call
-            let roomId = '';
-            if (doc) {
-                // Chatroom.find({chatroom_name: roomName}, function (err, doc) {
-                //     if (err) {
-                //         console.log(err);
-                //     } else {
-                //         roomId = doc[0].chatroom_id;
-                //         console.log(roomId);
-                //     }
-                // });
-                // roomId = docs.chatroom_id;
-                doc = Chatroom.find({chatroom_name: roomName}).exec()
-                // console.log(doc[0].chatroom_id)
+            let roomId;
+            // If a chatroom with that name exists get it, else create one
+            if (docs.length > 0) {
+                roomId = docs[0].chatroom_id;
             } else {
                 roomId = roomGenerator.roomIdGenerator()
                 const newChatroom = new Chatroom ({

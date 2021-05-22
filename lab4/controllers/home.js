d84e5f70a0d05699579e8f25a8cd5639fcb860c7
Juan Barrientos <jbarr078@ucr.edu>
2021-05-22 13:54:19 -0700

    Implemented rooms and updated home page
    Co-authored-by: elise-lin <elise-lin@users.noreply.github.com>

--- lab4/controllers/home.js
+++ lab4/controllers/home.js
@@ -1,37 +1,43 @@
 // Controller handler to handle functionality in home page
-const Chatrooms = require('../models/Chatrooms')
+const Chatrooms = require("../models/Chatrooms");
 // Example for handle a get request at '/' endpoint.
 fakeDB = () => {
   return [
     {
-      "chatroom_name": "room1"
+      chatroom_name: "room1",
     },
     {
-      "chatroom_name": "room2"
-    }
-  ]
-}
+      chatroom_name: "room2",
+    },
+  ];
+};
 
-function getHome(request, response){
+function getHome(request, response) {
   // do any work you need to do, then
   // response.render('home', {title: 'home'});
 
   // TODO: Get chatroom names from mongoose - probably a .find()
-  Chatrooms.find().lean().then(items => 
-    {
-      response.render('home', {title: 'home', rooms: items, isAvailable: true});
-    })
-  
+  Chatrooms.find()
+    .lean()
+    .then((items) => {
+      response.render("home", {
+        title: "home",
+        rooms: items,
+        isAvailable: true,
+      });
+    });
 }
-function getRoom (req, res) {
-  Chatrooms.find().lean().then(items => {
-    res.json(items)
-  })
+function getRoom(req, res) {
+  Chatrooms.find()
+    .lean()
+    .then((items) => {
+      res.json(items);
+    });
 }
 
-// function createRoom 
+// function createRoom
 
 module.exports = {
-    getHome,
-    getRoom
+  getHome,
+  getRoom,
 };
--- lab4/models/Messages.js
+++ lab4/models/Messages.js
@@ -1,25 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   nickname: {
     type: String,
-    required: true
+    required: true,
   },
   dateOfEntry: {
     type: Date,
-    default: Date.now()
+    default: new Date(),
   },
   text_body: {
     type: String,
-    required: true
-  },
-  chatroom_id: {
-      type: String,
-      required: true
+    required: true,
   },
   chatroom_name: {
-      type: String,
-      required: true
-  }
+    type: String,
+    required: true,
+  },
 });
-module.exports = Item = mongoose.model('messages', MessageSchema);
+module.exports = Item = mongoose.model("messages", MessageSchema);
